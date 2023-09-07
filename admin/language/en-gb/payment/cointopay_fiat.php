81dfcedd8113a883190e858e1705c4717cdbaaa1
Syed Namoos Ali Shah <namoosshah@gmail.com>
2023-09-07 19:01:15 +0500

    OpenCart 4 Cointopay Fiat Plugin

---
+++ admin/language/en-gb/payment/cointopay_fiat.php
@@ -0,0 +1,45 @@
+<?php
+// Heading
+$_['heading_title'] = 'Cointopay International B.V. Fiat';
+
+// Text
+$_['text_description'] = '<strong>Having trouble?</strong> Please submit the KYC/AML information on the Cointopay website in the account section. Only when your account is verified can you do shopping cart check outs and start receiving customer funds. Apologies but this is required for compliance reasons.';
+$_['text_edit'] = 'Edit Cointopay Fiat';
+$_['text_payment'] = 'Payment';
+$_['text_success'] = 'Success: You have modified Cointopay account details!';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+
+
+// Entry
+$_['entry_securitycode'] = 'SecurityCode';
+$_['entry_crypto_coin'] = 'Default Crypto Coin';
+$_['entry_redirect_url'] = 'Redirect URL';
+$_['entry_status'] = 'Status';
+$_['entry_display_name'] = 'Display Name';
+$_['entry_order_status'] = 'Order Status After Create Invoice';
+$_['entry_callback_success_order_status'] = 'Order Status After Callback success From Cointopay';
+$_['entry_callback_failed_order_status'] = 'Order Status After Callback Failed From Cointopay';
+$_['entry_callback_expired_order_status'] = 'Order Status After Callback Expired From Cointopay';
+$_['entry_callback_notenough_order_status'] = 'Order Status After Callback Underpaid From Cointopay';
+$_['entry_callback_cancel_order_status'] = 'Order Status when Order Cancel';
+$_['entry_merchantID'] = 'Merchant ID';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_api_key_hint'] = 'Get this at cointopay.com under Account.';
+$_['help_crypto_coin_hint'] = 'Default crypto coin selected at checkout.';
+$_['help_redirect_url_hint'] = 'The Redirect URL may be specified in your Cointopay.com Account.';
+$_['help_display_name_hint'] = 'The text that people see when making a purchase.';
+$_['help_merchantID_hint'] = 'Enter your ID from cointopay account';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify payment Cointopay!';
+$_['error_api_key'] = 'Please enter valid Security Code!';
+$_['error_display_name'] = 'Please enter Display Name!';
+$_['error_merchantID'] = 'Please enter Valid MerchantID!';
+$_['error_crypto_coin'] = 'Please enter correct MerchantID then Select Default Crypto Coin!';
+$_['error_invalid_currency'] = 'Your Store currency not supported. Please let us know.';
+
+// Tab
+$_['tab_general'] = 'General';
