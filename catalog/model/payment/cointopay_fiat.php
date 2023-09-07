80882027b5f35b0d26998e9d19f46f436aa758e2
Syed Namoos Ali Shah <namoosshah@gmail.com>
2023-09-07 20:40:53 +0500

    varible key fixed

--- catalog/model/payment/cointopay_fiat.php
+++ catalog/model/payment/cointopay_fiat.php
@@ -12,7 +12,7 @@ class CointopayFiat extends \Opencart\System\Engine\Model
 		} elseif (!$this->config->get('payment_cointopay_fiat_geo_zone_id')) {
 			$status = true;
 		} else {
-			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int) $this->config->get('payment_credit_card_geo_zone_id') . "' AND `country_id` = '" . (int) $address['country_id'] . "' AND (`zone_id` = '" . (int) $address['zone_id'] . "' OR `zone_id` = '0')");
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int) $this->config->get('payment_cointopay_fiat_geo_zone_id') . "' AND `country_id` = '" . (int) $address['country_id'] . "' AND (`zone_id` = '" . (int) $address['zone_id'] . "' OR `zone_id` = '0')");
 
 			if ($query->num_rows) {
 				$status = true;
